fc5b623b841efe94cd9066622d2797b3e65ebc3b
AMatsuda-midd <matsuda.aska@gmail.com>
2021-03-22 13:18:46 -0400

    Added in code from polling graphics.

--- drawspace.cpp
+++ drawspace.cpp
@@ -1,8 +1,51 @@
 #include "drawspace.h"
 #include <QSurface>
 
-drawspace::drawspace()
-{
+#include <QtWidgets>
 
+drawspace::drawspace() {
+    setScene(&mScene);
+    setSceneRect(QRectF(-400.0, -300.0, 800.0, 600.0));
+    setRenderHint(QPainter::Antialiasing);
+
+    QPainterPath crossPath;
+    crossPath.moveTo(-4.0, 0.0); crossPath.lineTo(-1.0, 0.0);
+    crossPath.moveTo(1.0, 0.0); crossPath.lineTo(4.0, 0.0);
+    crossPath.moveTo(0.0, -4.0); crossPath.lineTo(0.0, -1.0);
+    crossPath.moveTo(0.0, 1.0); crossPath.lineTo(0.0, 4.0);
+    crosshairs = mScene.addPath(crossPath, QPen(Qt::red, 1.0));
+    crosshairs->setVisible(false);
 }
-i
+
+void PollingGraphicsView::setCrosshairsVisible(bool visible) {
+    crosshairs->setVisible(visible);
+}
+
+void PollingGraphicsView::setCrosshairsPos(const QPointF &pos) {
+    crosshairs->setPos(pos);
+}
+
+void PollingGraphicsView::mousePressEvent(QMouseEvent *evt) {
+    QPointF pos = mapToScene(evt->pos());
+    emit mouseEvent(MousePressed, QDateTime::currentMSecsSinceEpoch(), pos);
+    lastPos = pos;
+}
+
+void PollingGraphicsView::maybeAddSegment(const QPointF &pos) {
+    if (lastPos!=pos) {
+        mScene.addLine(QLineF(lastPos, pos), QPen(Qt::black, 2.0));
+        lastPos = pos;
+    }
+}
+
+void PollingGraphicsView::mouseReleaseEvent(QMouseEvent *evt) {
+    QPointF pos = mapToScene(evt->pos());
+    emit mouseEvent(MouseReleased, QDateTime::currentMSecsSinceEpoch(), pos);
+    maybeAddSegment(pos);
+}
+
+void PollingGraphicsView::mouseMoveEvent(QMouseEvent *evt) {
+    QPointF pos = mapToScene(evt->pos());
+    emit mouseEvent(MouseMoved, QDateTime::currentMSecsSinceEpoch(), pos);
+    maybeAddSegment(pos);
+}
--- drawspace.h
+++ drawspace.h
@@ -1,24 +1,37 @@
 #ifndef DRAWSPACE_H
 #define DRAWSPACE_H
 
+#include <QGraphicsView>
+#include <QObject>
 #include <QWidget>
-#include <QPoint>
-#include <QColor>
 
+class QGraphicsPathItem;
 
-class drawspace : public QWidget
+class drawspace : public QGraphicsView
 {
     Q_OBJECT
 
-private:
-    void newPage();
-    void closeEvent(QCloseEvent *event) override;
-    void exportPage(QString fileType);
+    QGraphicsScene mScene;
+    QPointF lastPos;
+    QGraphicsPathItem *crosshairs;
 
+    void maybeAddSegment(const QPointF &pos);
 
 public:
+    enum { MousePressed, MouseReleased, MouseMoved };
+
     drawspace();
 
+    void setCrosshairsVisible(bool visible);
+    void setCrosshairsPos(const QPointF &pos);
+
+protected:
+    virtual void mousePressEvent(QMouseEvent *evt) override;
+    virtual void mouseReleaseEvent(QMouseEvent *evt) override;
+    virtual void mouseMoveEvent(QMouseEvent *evt) override;
+
+signals:
+    void mouseEvent(int type, int when, QPointF pos);
 };
 
 #endif // DRAWSPACE_H
