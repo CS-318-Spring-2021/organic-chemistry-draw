1bf42ace6ef800415c860535223a75502bd77bfa
sam-dewolf <62413716+sam-dewolf@users.noreply.github.com>
2021-05-18 13:51:41 -0400

    Implements save functionality into a .png format

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,15 +27,11 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {
 
     QVBoxLayout *taskBarLayout = new QVBoxLayout();
     taskBarLayout->addWidget(recordCheckBox = new QCheckBox("Record"));
-    taskBarLayout->addWidget(undoCheckBox = new QCheckBox("Undo"));
 
     recordCheckBox->setCheckable(true);
     recordCheckBox->setChecked(true);
 
     connect(recordCheckBox, &QCheckBox::stateChanged, this, &MainWindow::bRecording);
-
-    undoCheckBox->setCheckable(true);
-    undoCheckBox->setChecked(false);
 
     rightLayout->addWidget(undoButton = new QPushButton("Undo"));
     connect(undoButton, &QPushButton::clicked, this, &MainWindow::bUndo);
@@ -57,27 +53,17 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::bSave() {
-    QString fName = QFileDialog::getSaveFileName(this, "Enter save filename", QString(), tr("Comma-separated values (*.csv)"));
+    QString date = QDate::currentDate().toString("MM-dd-yyyy");
+    QString fName = QFileDialog::getSaveFileName(this, "Enter save filename", date, tr("Image File (*.png)"));
     if (fName.isEmpty()) return;
-
     QFile fOut(fName);
     if (!fOut.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
         QMessageBox::warning(this, "Error opening file", "Can't open selected file for output.");
         return;
     }
 
-
-    QTextStream out(&fOut);
-    out << "t,dt,x,y,speed,acceleration\n";
-    /*
-    for (int row = 0; row < table->rowCount(); ++row) {
-        out << table->item(row, 0)->text() << "," << table->item(row, 1)->text() << "," << table->item(row, 2)->text() << "," << table->item(row, 3)->text();
-        QString note = table->item(row, 4)->text();
-        if (!note.isEmpty()) out << "," << note;
-        out << table->item(row, 5)->text();
-        out << "\n";
-    */
-
+    QPixmap pixMap = view->grab();
+    pixMap.save(fName);
     return;
 }
 
